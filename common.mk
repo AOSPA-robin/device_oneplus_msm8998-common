22db4dd0e88b546c77da4f4272472bb5a370c7dd
NurKeinNeid <mralexman3000@gmail.com>
2018-12-02 13:09:36 +0100

    msm8998-common: AOSiP/DerpFest bringup
     * Setup makefiles and dependencies for AOSiP
     * Update extract-utils for AOSiP
     * Track PixelExperience GApps and Mods
     * Introduce KronicKernel

--- common.mk
+++ common.mk
@@ -143,7 +143,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
-    Snap2
+    PlaygroundMod \
+    WallpapersBReel2018
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
